b0e6727151cb240dda3243db828e2183218db70b
ahamedzoha <ahamed.zoha@gmail.com>
2023-07-02 13:46:22 +0600

    implemented global auth guard and Public param for public routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppService } from "./app.service"
 import { DatabaseConnectionModule } from "./database-connection/database-connection.module"
 import { ConfigModule } from "./config/config.module"
 import { UsersModule } from "./users/users.module"
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module"
+import { JwtAtGuard } from "./auth/guards/jwt-at.guard"
 
 @Module({
   imports: [ConfigModule, DatabaseConnectionModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: "APP_GUARD",
+      useClass: JwtAtGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,39 +11,40 @@ import { AuthService } from "./auth.service"
 import { LoginUserDto } from "./dtos/login-user.dto"
 import { LogoutUserDto } from "./dtos/logout-user.dto"
 import { TokenizedUser } from "./types/tokenized-user.types"
-import { JwtAtGuard } from "./guards/jwt-at.guard"
 import { JwtRtGuard } from "./guards/jwt-rt.guard"
 import { GetUser } from "./decorators/get-user.decorator"
 import { RefreshUserDto } from "./dtos/refresh-user.dto"
+import { Public } from "./decorators/public.decorator"
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post("local/signin")
   login(@Body() loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto)
   }
 
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   @Post("local/signup")
   async signup(@Body() createUserDto: CreateUserDto): Promise<TokenizedUser> {
     return this.authService.signup(createUserDto)
   }
 
-  @UseGuards(JwtAtGuard)
   @HttpCode(HttpStatus.OK)
   @Post("logout")
   async logout(@GetUser() user: LogoutUserDto) {
     return this.authService.logout(user)
   }
 
+  @Public()
   @UseGuards(JwtRtGuard)
   @HttpCode(HttpStatus.OK)
   @Post("refresh")
   async refresh(@GetUser() user: RefreshUserDto) {
-    console.log(user)
     return this.authService.refresh(user.sub, user.refreshToken)
   }
 }
